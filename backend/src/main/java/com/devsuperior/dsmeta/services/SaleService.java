8ad7111495888122d993a88416020daef06c64a9
João Guilherme <vpguisilva@icloud.com>
2022-07-17 18:05:27 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dsmeta.servicesSale;
+package com.devsuperior.dsmeta.services;
 
 import java.time.Instant;
 import java.time.LocalDate;
@@ -17,14 +17,14 @@ public class SaleService {
 
 	@Autowired
 	private SaleRepository repository;
-	
+
 	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
-		
+
 		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
-		
-		LocalDate min = minDate.equals("")? today.minusDays(365) : LocalDate.parse(minDate);
-		LocalDate max = maxDate.equals("")? today : LocalDate.parse(maxDate);
-		
+
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
 		return repository.findSales(min, max, pageable);
 	}
 }
